8836b5b5c40f736a83982e57466fbbd10f86c47e
Tibor Áser Veres (RPBCACUEAIIBH) <v.tibor.a@gmail.com>
2018-07-25 00:55:32 +0300

    Initial commit.

---
+++ BashTool.sh
@@ -0,0 +1,522 @@
+#! /bin/bash
+Version="1.0"
+Answer=""
+Error=false
+Done=false
+RandomNumber=""
+Mark=true
+
+function IndentLines
+{
+  LineCount
+  touch ${File}Temp
+  LN=1
+  while [[ $LN -le $LC ]]
+  do
+    if [[ $LN -ge $FromLine && $LN -le $ToLine ]]
+    then
+      X=0
+      while [[ $X -lt $Indent ]]
+      do
+        echo -n "  " >> ${File}Temp
+        X=$(( $X + 1 ))
+      done
+      sed "${LN}q;d" $File >> ${File}Temp
+    else
+      sed "${LN}q;d" $File >> ${File}Temp
+    fi
+    LN=$(( $LN + 1 ))
+  done
+  mv ${File}Temp ${File}
+  FromLine=""
+  ToLine=""
+}
+
+function CommentLines
+{
+  LineCount
+  touch ${File}Temp
+  LN=1
+  while [[ $LN -le $LC ]]
+  do
+    if [[ $LN -ge $FromLine && $LN -le $ToLine ]]
+    then
+      echo -n "#" >> ${File}Temp
+      sed "${LN}q;d" $File >> ${File}Temp
+    else
+      sed "${LN}q;d" $File >> ${File}Temp
+    fi
+    LN=$(( $LN + 1 ))
+  done
+  mv ${File}Temp ${File}
+  FromLine=""
+  ToLine=""
+}
+
+function UncommentLines
+{
+  LineCount
+  touch ${File}Temp
+  LN=1
+  while [[ $LN -le $LC ]]
+  do
+    if [[ $LN -ge $FromLine && $LN -le $ToLine ]]
+    then
+      LineContent=$(sed "${LN}q;d" $File)
+      if [[ ${LineContent:0:1} == "#" ]]
+      then
+        echo "${LineContent:1:100000}" >> ${File}Temp
+      else
+        echo "$LineContent" >> ${File}Temp
+      fi
+    else
+      sed "${LN}q;d" $File >> ${File}Temp
+    fi
+    LN=$(( $LN + 1 ))
+  done
+  mv ${File}Temp ${File}
+  FromLine=""
+  ToLine=""
+}
+
+function LineCount
+{
+  LC=0
+  while read -r p
+  do
+    LC=$(( $LC + 1 ))
+  done < $File
+}
+
+function Disassemble
+{
+  FromLine=$AtLine
+  LineCount
+  ToLine=$LC
+  Mark=false
+  HideLines
+}
+
+function Reassemble
+{
+  Marker=$RandomNumber
+  Unhide
+  Mark=true
+}
+
+function DoIndent
+{
+  X=0
+  while [[ $X -lt $Indent ]]
+  do
+    echo -n "  " >> $File
+    X=$(( $X + 1 ))
+  done
+}
+
+function HideLines
+{
+  RandomSelection=true
+  while [[ $RandomSelection == true ]]
+  do
+    RandomNumber=$RANDOM
+    if [[ ! -e ./Hidden/Hidden$RandomNumber ]]
+    then
+      RandomSelection=false
+    fi
+  done
+  touch ./Hidden/Hidden$RandomNumber
+  LN=$FromLine
+  while [[ $LN -le $ToLine ]]
+  do
+    sed "${LN}q;d" $File >> ./Hidden/Hidden$RandomNumber
+    LN=$(( $LN + 1 ))
+  done
+  LineCount
+  touch ${File}Temp
+  LN=1
+  while [[ $LN -le $LC ]]
+  do
+    if [[ $LN -ge $FromLine && $LN -le $ToLine ]]
+    then
+      if [[ $LN -eq $FromLine ]]
+      then
+        if [[ $Mark == true ]]
+        then
+          echo "# $(( $ToLine - $FromLine + 1 )) line(s) hiddebn by BashTool! Marker: $RandomNumber" >> ${File}Temp
+        fi
+      fi
+    else
+      sed "${LN}q;d" $File >> ${File}Temp
+    fi
+    LN=$(( $LN + 1 ))
+  done
+  mv ${File}Temp ${File}
+  FromLine=""
+  ToLine=""
+}
+
+function Unhide
+{
+  if [[ $Marker == "all" ]]
+  then
+    while [[ ! -z $(grep 'line(s) hiddebn by BashTool! Marker:' $File) ]]
+    do
+      LineCount
+      LN=1
+      touch ${File}Temp
+      while [[ $LN -le $LC ]]
+      do
+        if [[ ! -z $(sed "${LN}q;d" $File | grep 'line(s) hiddebn by BashTool! Marker:') ]]
+        then
+          ThisMarker=$(sed "${LN}q;d" $File | awk '{ print $8 }')
+          cat "./Hidden/Hidden$ThisMarker" >> "${File}Temp"
+          rm ./Hidden/Hidden$ThisMarker
+        else
+          sed "${LN}q;d" $File >> ${File}Temp
+        fi
+        LN=$(( $LN + 1 ))
+      done
+      mv ${File}Temp ${File}
+    done
+  else
+    LineCount
+    LN=1
+    touch ${File}Temp
+    while [[ $LN -le $LC ]]
+    do
+      if [[ ! -z $(sed "${LN}q;d" $File | grep 'line(s) hiddebn by BashTool! Marker:') ]]
+      then
+        ThisMarker=$(sed "${LN}q;d" $File | awk '{ print $8 }')
+        if [[ $ThisMarker -eq $Marker ]]
+        then
+          cat ./Hidden/Hidden$ThisMarker >> "${File}Temp"
+          rm ./Hidden/Hidden$ThisMarker
+        else
+          sed "${LN}q;d" $File >> ${File}Temp
+        fi
+      else
+        sed "${LN}q;d" $File >> ${File}Temp
+      fi
+      LN=$(( $LN + 1 ))
+    done
+    mv ${File}Temp ${File}
+  fi
+  Marker=""
+}
+
+# Program starts here
+while [[ $Done == false ]]
+do
+  read -p 'Please specify the file you want to work with: ' File
+  if [[ -f "$File" ]]
+  then
+    Done=true
+    echo "$File Selected!"
+  fi
+done
+while [[ $Answer != "exit" ]]
+do
+  if [[ $Error == true ]]
+  then
+    echo "Error: $Answer is an unknown command!"
+    Error=false
+  fi
+  if [[ $Answer == "help" ]]
+  then
+    echo "if [line number] [indent depth (number)] - Inserts an if statement."
+    echo "ife [line number] [indent depth (number)] - Inserts an if-else statement."
+    echo "for [line number] [indent depth (number)] - Inserts a for loop."
+    echo "while [line number] [indent depth (number)] - Inserts a while loop."
+    echo "case [line number] [indent depth (number)] - Inserts a case statement."
+    echo "function [line number] - Inserts an empty function."
+    echo ""
+    echo "hide [from line number] [to line number] - Hide lines and replace with a marker."
+    echo "unhide [Number of marker] - Unhides lines marked by that marker only."
+    echo "unhide - Unhides all hidden lines. (Run this before trying to run your script!)"
+    echo "comment [from line number] [to line number] - Comment lines"
+    echo "uncomment [from line number] [to line number] - Uncomment lines"
+    echo "indent [from line number] [to line number] [indent depth (number)] - Indents lines"
+    echo ""
+    echo "ql - Quick list of useful command structures available for insertion."
+    echo "templates - Lists the available templates. (This can be useful when starting new script.)"
+    echo ""
+    echo "version - Shows current version."
+    echo "help - Shows these options."
+    echo "exit - Ends the loop..."
+    echo ""
+  fi
+  if [[ $Answer == "ql" ]]
+  then
+    echo "Sorry! ...not implemented yet."
+    echo ""    
+  fi
+  if [[ $Answer == "templates" ]]
+  then
+    echo "Sorry! ...not implemented yet."
+    echo ""
+    echo "Soon to be available templates: "
+    echo "- Bare minimum"
+    echo "- Interactive (Like this tool... it can be useful for controling another script by changing variables in files...)"
+    echo "- MultiThreaded (An advanced template that detects cpu cores and user, launches and manages threads, and if it's running as root it also sets task priority for higher efficiency.)"
+    echo ""
+  fi
+  read -p 'Type "help" to list options. Waiting for orders: ' Answer
+  case $(echo $Answer | awk '{ print $1 }')
+  in
+          "hide" ) FromLine=$(echo $Answer | awk '{ print $2 }')
+                   ToLine=$(echo $Answer | awk '{ print $3 }')
+                   LineCount
+                   if [[ $FromLine -gt 0 && $FromLine -le $LC ]]
+                   then
+                     if [[ -z $ToLine || $ToLine -gt $FromLine && $ToLine -le $LC ]]
+                     then
+                       if [[ -z $ToLine ]]
+                       then
+                         ToLine=$FromLine
+                       fi
+                       HideLines
+                     fi
+                   fi
+                   ;;
+        "unhide" ) if [[ -z $(echo $Answer | awk '{ print $2 }') || $(echo $Answer | awk '{ print $2 }') == "all" ]]
+                   then
+                     Marker="all"
+                     Unhide
+                   else
+                     Marker=$(echo $Answer | awk '{ print $2 }')
+                     if [[ $Marker -gt -1 ]]
+                     then
+                       Unhide
+                     fi
+                   fi
+                   ;;
+          "File" ) Done=false
+                   while [[ $Done == false ]]
+                   do
+                     read -p 'Please specify the file you want to work with or type "cancel" not to change anything!: ' X
+                     if [[ -f "$X" || $X == "cancel" ]]
+                     then
+                       if [[ $X != "cancel" ]]
+                       then
+                         File="$X"
+                         Done=true
+                         echo "$File Selected!"
+                       fi
+                     fi
+                   done
+                   ;;
+            "if" ) LineCount
+                   AtLine=$(echo $Answer | awk '{ print $2 }')
+                   if [[ $AtLine -gt 0 && $AtLine -le $LC ]]
+                   then
+                     if [[ ! -z $(echo $Answer | awk '{ print $3 }') && $(echo $Answer | awk '{ print $3 }') -gt 0 ]]
+                     then
+                       Indent=$(echo $Answer | awk '{ print $3 }')
+                     else
+                       Indent=0
+                     fi
+                     Disassemble
+                     DoIndent
+                     echo 'if [[  ]]' >> $File
+                     DoIndent
+                     echo 'then' >> $File
+                     DoIndent
+                     echo '  :' >> $File
+                     DoIndent
+                     echo 'fi' >> $File
+                     echo "# X line(s) hiddebn by BashTool! Marker: $RandomNumber" >> $File
+                     Reassemble
+                   fi
+                   ;;
+           "ife" ) LineCount
+                   AtLine=$(echo $Answer | awk '{ print $2 }')
+                   if [[ $AtLine -gt 0 && $AtLine -le $LC ]]
+                   then
+                     if [[ ! -z $(echo $Answer | awk '{ print $3 }') && $(echo $Answer | awk '{ print $3 }') -gt 0 ]]
+                     then
+                       Indent=$(echo $Answer | awk '{ print $3 }')
+                     else
+                       Indent=0
+                     fi
+                     Disassemble
+                     DoIndent
+                     echo 'if [[  ]]' >> $File
+                     DoIndent
+                     echo 'then' >> $File
+                     DoIndent
+                     echo '  :' >> $File
+                     DoIndent
+                     echo 'else' >> $File
+                     DoIndent
+                     echo '  :' >> $File
+                     DoIndent
+                     echo 'fi' >> $File
+                     echo "# X line(s) hiddebn by BashTool! Marker: $RandomNumber" >> $File
+                     Reassemble
+                   fi
+                   ;;
+           "for" ) LineCount
+                   AtLine=$(echo $Answer | awk '{ print $2 }')
+                   if [[ $AtLine -gt 0 && $AtLine -le $LC ]]
+                   then
+                     if [[ ! -z $(echo $Answer | awk '{ print $3 }') && $(echo $Answer | awk '{ print $3 }') -gt 0 ]]
+                     then
+                       Indent=$(echo $Answer | awk '{ print $3 }')
+                     else
+                       Indent=0
+                     fi
+                     Disassemble
+                     DoIndent
+                     echo 'for $i in $This' >> $File
+                     DoIndent
+                     echo 'do' >> $File
+                     DoIndent
+                     echo '  :' >> $File
+                     DoIndent
+                     echo 'done' >> $File
+                     echo "# X line(s) hiddebn by BashTool! Marker: $RandomNumber" >> $File
+                     Reassemble
+                   fi
+                   ;;
+         "while" ) LineCount
+                   AtLine=$(echo $Answer | awk '{ print $2 }')
+                   if [[ $AtLine -gt 0 && $AtLine -le $LC ]]
+                   then
+                     if [[ ! -z $(echo $Answer | awk '{ print $3 }') && $(echo $Answer | awk '{ print $3 }') -gt 0 ]]
+                     then
+                       Indent=$(echo $Answer | awk '{ print $3 }')
+                     else
+                       Indent=0
+                     fi
+                     Disassemble
+                     DoIndent
+                     echo 'while [[  ]]' >> $File
+                     DoIndent
+                     echo 'do' >> $File
+                     DoIndent
+                     echo '  :' >> $File
+                     DoIndent
+                     echo '# X=$(( $X + 1 ))' >> $File
+                     DoIndent
+                     echo 'done' >> $File
+                     echo "# X line(s) hiddebn by BashTool! Marker: $RandomNumber" >> $File
+                     Reassemble
+                   fi
+                   ;;
+          "case" ) LineCount
+                   AtLine=$(echo $Answer | awk '{ print $2 }')
+                   if [[ $AtLine -gt 0 && $AtLine -le $LC ]]
+                   then
+                     if [[ ! -z $(echo $Answer | awk '{ print $3 }') && $(echo $Answer | awk '{ print $3 }') -gt 0 ]]
+                     then
+                       Indent=$(echo $Answer | awk '{ print $3 }')
+                     else
+                       Indent=0
+                     fi
+                     Disassemble
+                     DoIndent
+                     echo 'case $X in' >> $File
+                     DoIndent
+                     echo '          "" ) :' >> $File
+                     DoIndent
+                     echo '               ;;' >> $File
+                     DoIndent
+                     echo '            *) echo "Error: Unknown option at: case $X!"' >> $File
+                     DoIndent
+                     echo '               ;;' >> $File
+                     DoIndent
+                     echo 'esac' >> $File
+                     echo "# X line(s) hiddebn by BashTool! Marker: $RandomNumber" >> $File
+                     Reassemble
+                   fi
+                   ;;
+      "function" ) LineCount
+                   AtLine=$(echo $Answer | awk '{ print $2 }')
+                   if [[ $AtLine -gt 0 && $AtLine -le $LC ]]
+                   then
+                     if [[ ! -z $(echo $Answer | awk '{ print $3 }') && $(echo $Answer | awk '{ print $3 }') -gt 0 ]]
+                     then
+                       Indent=$(echo $Answer | awk '{ print $3 }')
+                     else
+                       Indent=0
+                     fi
+                     Disassemble
+                     DoIndent
+                     echo 'function RENAME' >> $File
+                     DoIndent
+                     echo '{' >> $File
+                     DoIndent
+                     echo '  :' >> $File
+                     DoIndent
+                     echo '}' >> $File
+                     echo "# X line(s) hiddebn by BashTool! Marker: $RandomNumber" >> $File
+                     Reassemble
+                   fi
+                   ;;
+       "comment" ) FromLine=$(echo $Answer | awk '{ print $2 }')
+                   ToLine=$(echo $Answer | awk '{ print $3 }')
+                   LineCount
+                   if [[ $FromLine -gt 0 && $FromLine -le $LC ]]
+                   then
+                     if [[ -z $ToLine || $ToLine -gt $FromLine && $ToLine -le $LC ]]
+                     then
+                       if [[ -z $ToLine ]]
+                       then
+                         ToLine=$FromLine
+                       fi
+                       CommentLines
+                     fi
+                   fi
+                   ;;
+     "uncomment" ) FromLine=$(echo $Answer | awk '{ print $2 }')
+                   ToLine=$(echo $Answer | awk '{ print $3 }')
+                   LineCount
+                   if [[ $FromLine -gt 0 && $FromLine -le $LC ]]
+                   then
+                     if [[ -z $ToLine || $ToLine -gt $FromLine && $ToLine -le $LC ]]
+                     then
+                       if [[ -z $ToLine ]]
+                       then
+                         ToLine=$FromLine
+                       fi
+                       UncommentLines
+                     fi
+                   fi
+                   ;;
+        "indent" ) FromLine=$(echo $Answer | awk '{ print $2 }')
+                   ToLine=$(echo $Answer | awk '{ print $3 }')
+                   LineCount
+                   if [[ $FromLine -gt 0 && $FromLine -le $LC ]]
+                   then
+                     if [[ -z $ToLine || $ToLine -gt $FromLine && $ToLine -le $LC ]]
+                     then
+                       if [[ -z $ToLine ]]
+                       then
+                         ToLine=$FromLine
+                       fi
+                       if [[ ! -z $(echo $Answer | awk '{ print $4 }') && $(echo $Answer | awk '{ print $4 }') -gt 0 ]]
+                       then
+                         Indent=$(echo $Answer | awk '{ print $4 }')
+                       else
+                         Indent=0
+                       fi
+                       IndentLines
+                     fi
+                   fi
+                   ;;
+            "ql" ) :
+                   ;;
+     "templates" ) :
+                   ;;
+       "version" ) echo "Version $Version"; sleep 2
+                   ;;
+          "help" ) :
+                   ;;
+          "exit" ) :
+                   ;;
+                *) Error=true
+                   ;;
+  esac
+  clear
+done
---
+++ README.md
@@ -0,0 +1,26 @@
+BashTool
+Version 1.0
+
+Interactive script for adding IDE functionality to gedit or any other text editor capable of recognizin changed content and asking to reload... Only for bash scripts...
+
+Features:
+- Adding if, if-else, case
+- Adding for, while loops
+- Adding function
+- Hiding/unhiding/commenting/uncommenting/indenting lines
+- Quick list of insertable useful command structures
+- Bash script templates
+
+Operation:
+- Create an empty file
+- Open gedit on one side of the screen
+- Open a terminal and run the BashTool.sh script on the other side of the screen.
+- The script will ask for a file to work with, give it the empty file you opened in gedit.
+- Type "templates" and hit enter, to see the template options. Pick one, type the name and hit enter.
+- Click on the gedit window... It should detect that the file has been changed, and ask if you want to reload it. Click reload...
+- Typing "help" in the terminal and hitting enter will give you another list of options, and the rest is pretty self explanatory...
+
+Some things to note:
+- The script may not be able to do anything else but add templates with single line files... The edited file needs to have at least 2 files to function properly... (This may be fixed later...)
+- You must unhide hidden lines before running your script. That's because it actually replaces the lines with the marker saving them in the "Hidden" directory within the BashTool folder... Typing "unhide" with no marker specified will reconstruct the file recursively till there are no markers left. That should work even with hidden markers...
+- You must save the file before typing in the terminal, so that you don't loose the changes when you reload the file!
